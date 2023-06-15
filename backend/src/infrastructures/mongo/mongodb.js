e60ca3b4141eafde5a93d75309b53751f9b35cea
Charles <charles.gam@outlook.com>
2023-06-15 15:02:53 +0200

    commentaire url bdd

--- backend/src/infrastructures/mongo/mongodb.js
+++ backend/src/infrastructures/mongo/mongodb.js
@@ -1,6 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://root:example@localhost:27017/', {
+// !!!!! URL mis en dur pour le test mais l'utilisation de variables d'environment recommandé
+mongoose
+  .connect("mongodb://root:example@localhost:27017/", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("Mongo connected..."))
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongo connected..."));
